9850e54eb1ad42da605a2908ebf443fdb2ba84ae
carlalennon <99259113+carlalennon@users.noreply.github.com>
2024-05-25 00:29:36 +0100

    Spell check turns text green/red

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
 const poemDisplay = document.getElementById('poem');
+const poemInput = document.getElementById('poem-type');
 
-function generatePoem() {
-    fetch('poems.json')
+poemInput.addEventListener('input', () => {
+    const arrayPoem = poemDisplay.querySelectorAll('span');
+    const arrayValue = poemInput.value.split('');
+
+    arrayPoem.forEach((characterSpan, index) => {
+        const arrayPoem = poemDisplay.querySelectorAll('span');
+        const arrayValue = poemInput.value.split('');
+        arrayPoem.forEach((characterSpan, index) => {
+            const character = arrayValue[index];
+            if (character == null) {
+                characterSpan.classList.remove('correct');
+                characterSpan.classList.remove('incorrect');
+            } else if (character == characterSpan.innerText) {
+                characterSpan.classList.add('correct');
+                characterSpan.classList.remove('incorrect');
+            } else {
+                characterSpan.classList.remove('correct');
+                characterSpan.classList.add('incorrect');
+            }
+        });
+    });
+});
+
+function getPoem() {
+    return fetch('poems.json')
         .then(response => response.json())
-        .then(data => {
-            const poemBody = data.danta["1"].poem;
-            poemDisplay.innerHTML = poemBody.replace(/\n/g, '<br>'); // replace newline characters with HTML line breaks for proper display
-            console.log(poemBody);
-        })
+        .then(data => data.danta["2"].poem)
         .catch(error => {
             console.error('Error:', error);
         });
 }
 
-generatePoem();
+function displayPoem(poem) {
+    poemDisplay.innerHTML = ''; // Clear the poemDisplay
+    poem.split("").forEach(character => {
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character === '\n' ? '\n' : character; // Replace newline characters with a space
+        poemDisplay.appendChild(characterSpan);
+    });
+}
+
+getPoem().then(poem => displayPoem(poem));
